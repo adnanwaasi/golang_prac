005fa755b25091f642c8de4081a27868382a4a0e
credential.helper <adnanwaasi1@gmail.com>
2026-05-16 20:16:38 +0530

    read and write locks yes i was busy

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	wg      = sync.WaitGroup{}
-	locker  = sync.Mutex{}
+	locker  = sync.RWMutex{}
 	db      = []string{"waasi", "prajol", "shan10", "salih", "dark"}
 	results = []string{}
 )
@@ -20,16 +20,27 @@ func main() {
 		go readandwrite(i)
 	}
 	wg.Wait()
-	fmt.Printf("total time for execution is %v", time.Since(t))
+	fmt.Printf("\ntotal time for execution is %v", time.Since(t))
 	fmt.Println(results)
 }
 
 func readandwrite(i int) {
-	/*var delay float32 = 2000
-	time.Sleep(time.Duration(delay) * time.Millisecond)*/
-	fmt.Println("results from db are ", db[i])
-	locker.Lock()
-	results = append(results, db[i])
-	locker.Unlock()
+	var delay float32 = 2000
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	fmt.Println("\nresults from db are ", db[i])
+	write(db[i])
+	Log()
 	wg.Done()
+}
+
+func write(s string) {
+	locker.Lock()
+	results = append(results, s)
+	locker.Unlock()
+}
+
+func Log() {
+	locker.RLock()
+	fmt.Printf("\nthe results are %v", results)
+	locker.RUnlock()
 }
