a249fbe81e51969ea9271595b1fd3e3f501d7ffa
credential.helper <adnanwaasi1@gmail.com>
2026-05-19 21:12:49 +0530

    when you add a fn then it sends and receives simulteneously

--- channels/channels.go
+++ channels/channels.go
@@ -3,15 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	/*
+	
 	var c= make(chan int)
-	c <- 1
-	i := <-c
-	fmt.Println(i)
-	unbuffered channel and deadlock
-	why ? 
-	well 
-	it waits for someone (fn) to receive 
-	well there's no other go routine so it just waits forever like how you waited for her 
-	*/
+	go process(c)
+	for i:= range c{
+		fmt.Println(i)
+
+	}
+}	
+func process(c chan int ){
+	defer close(c)
+	for i:=0; i<5;i++{
+		c<-i    
+	}
 }
